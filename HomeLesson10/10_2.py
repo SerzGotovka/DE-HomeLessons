72123a5aab99499f245e7b0e099d452e0ef0fd9d
serik680 <serzpython@gmail.com>
2025-03-17 12:37:36 +0300

    В задачу 2 добавлена конструкция try/except

--- HomeLesson10/10_2.py
+++ HomeLesson10/10_2.py
@@ -16,39 +16,50 @@
 Сохраняет очищенные данные в новый файл cleaned_data.txt.
 """
 
-def transform_data(filename):
-    
-    with open(filename, 'r', encoding='utf-8') as file:
-        content = file.readlines()
-        transform_data = list()
 
-        for line in content:
-           transform_data.append(line.strip().split(':'))
-            
-        
-        return transform_data
+def transform_data(filename):
+    """Функция для чтения и преобразования файла"""
+    try:
+        with open(filename, "r", encoding="utf-8") as file:
+            content = file.readlines()
+            transform_data = list()
+
+            for line in content:
+                transform_data.append(line.strip().split(":"))
+
+            return transform_data
+    except FileNotFoundError:
+        print("Файл не найден!")
+    except:
+        print("Ошибка при работе с файлом!")
 
 
 def cleaned_data(data):
-    cleaned_data = list()
-    
-    for i in data:
-        if not 'ERROR' in i:
-            cleaned_data.append(i)
-        else:
-            continue
+    """Функция для очистки данных"""
+    try:
+        cleaned_data = list()
 
-    return cleaned_data
+        for i in data:
+            if not "ERROR" in i:
+                cleaned_data.append(i)
+            else:
+                continue
+
+        return cleaned_data
+    except:
+        print("Ошибка получения очищенных данных")
 
 
 def write_cleaned_data(filename, cleaned_data):
     """Функция для записи очищенных данных в файл"""
+    try:
+        with open(filename, "w", encoding="utf-8") as file:
+            for item in cleaned_data:
+                file.write(f"{item[0]}:{item[1]}:{item[2]}\n")
+    except:
+        print("Ошибка с созданием файла!")
 
-    with open(filename, 'w', encoding='utf-8') as file:
-        for item in cleaned_data:
-            file.write(f'{item[0]}:{item[1]}:{item[2]}\n')
-         
-        
-norm_data = transform_data('HomeLesson10/raw_data.txt')
+
+norm_data = transform_data("HomeLesson10/raw_data.txt")
 clean_data = cleaned_data(norm_data)
-write_file = write_cleaned_data('HomeLesson10/cleaned_data.txt', clean_data)
+write_file = write_cleaned_data("HomeLesson10/cleaned_data.txt", clean_data)
