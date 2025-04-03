42cde1db10e1bed82cbf119ce4db02a3d1dcc206
serik680 <serzpython@gmail.com>
2025-04-03 18:10:25 +0300

    в utils добавлена функция чтения и записи таблиц из sqlite

--- utils/scripts.py
+++ utils/scripts.py
@@ -1,3 +1,7 @@
+import sqlite3
+import pandas as pd
+from typing import Dict
+
 def read_file(filename: str) -> list:
     """Функция для чтения файла"""
     try:
@@ -20,3 +24,21 @@ def write_files(filename: str, data: list) -> None:
                 file.write(f"{str(item)}, ")
     except Exception as e:
         print(f"Ошибка с созданием файла: {e}")    
+
+
+def load_data_from_db(db_path: str) -> Dict:
+    """Загружает данные из базы данных SQLite и сохраняет их в словаре DataFrame."""
+    dataframes = {}
+    try:
+        with sqlite3.connect(db_path) as conn:
+            tables_query = "SELECT name FROM sqlite_master WHERE type='table';"
+            name_tables = pd.read_sql(tables_query, conn)
+            names_list = name_tables["name"].tolist()
+
+            for name in names_list:
+                df = pd.read_sql(f"SELECT * FROM {name}", conn)
+                dataframes[name] = df
+                df.to_excel(f"data/{name}.xlsx", index=False)  # Сохраняем в Excel
+    except Exception as e:
+        print(f"Ошибка при загрузке данных из базы: {e}")
+    return dataframes
