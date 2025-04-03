e40b86949d0f5895c8add42dadd5e9146ff4740d
serik680 <serzpython@gmail.com>
2025-04-03 19:50:01 +0300

    задача 1 решена

--- HomeLesson15/15_1.sql
+++ HomeLesson15/15_1.sql
@@ -23,8 +23,10 @@
 - Задание 1. Создайте таблицы
 - Задание 2. Заполните таблицы данными, не менее 5 строчек
 - Задание 3. Измените email студента с фамилией "<Имя>"
-- Задание 4. Удалите регистрацию студента <Имя> на курс "Базы данных":
+- Задание 4. Удалите регистрацию студента <Имя> на курс <Имя курса>:
 
+
+-- Задание 1. Создайте таблицы
 CREATE TABLE students(
 	id INTEGER PRIMARY KEY AUTOINCREMENT,
 	first_name VARCHAR(100),
@@ -53,6 +55,40 @@ CREATE TABLE registrations(
 );
 
 
+-- Задание 2. Заполните таблицы данными, не менее 5 строчек
+INSERT INTO students (first_name, last_name, email, date_receipt)
+VALUES 
+('Иван', 'Иванов', 'ivan.ivanov@example.com', '2023-09-01'),
+('Мария', 'Петрова', 'maria.petrova@example.com', '2023-09-01'),
+('Алексей', 'Сидоров', 'alexey.sidorov@example.com', '2023-09-01'),
+('Ольга', 'Кузнецова', 'olga.kuznetsova@example.com', '2023-09-01'),
+('Дмитрий', 'Смирнов', 'dmitry.smirnov@example.com', '2023-09-01');
+
+INSERT INTO courses (name_course, name_teacher, credit) VALUES
+('Программирование на Python', 'Анна Васильева', 4.0),
+('JAVA', 'Сергей Петров', 3.0),
+('Алгоритмы и структуры данных', 'Елена Николаева', 5.0),
+('Веб-разработка', 'Игорь Сидоров', 4.0),
+('Машинное обучение', 'Мария Кузнецова', 5.0);
+
+INSERT INTO registrations (id_student, id_course) VALUES
+(1, 1),  
+(1, 2), 
+(2, 1), 
+(3, 3),  
+(4, 4), 
+(5, 5); 
+
+
+-- Задание 3. Измените email студента с фамилией "<Имя>"
+UPDATE students
+SET email='123345@google.com' WHERE last_name='Сидоров';
+
+
+-- Задание 4. Удалите регистрацию студента <Имя> на курс <Имя курса>":
+DELETE FROM registrations 
+WHERE id_student = (select id from students where first_name = 'Иван')
+and id_course = (select id from courses where name_course = 'Программирование на Python');
 
 
 
